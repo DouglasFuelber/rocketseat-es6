24458eda3aa86bc9b6847ff1e3be2d706dbd8336
DouglasFuelber <dffuelber@gmail.com>
2019-10-24 13:12:27 -0300

    Module 2: Exercises

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    entry: ['./src/main.js', './src/exercises_module_1.js'],
+    entry: ['./src/main.js', './src/exercises_module_1.js', './src/exercises_module_2.js'],
     output: {
         path: __dirname + '/public',
         filename: 'bundle.js',
